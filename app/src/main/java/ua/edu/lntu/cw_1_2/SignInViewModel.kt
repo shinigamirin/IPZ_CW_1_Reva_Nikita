00babde11741b850c63ea76dacee713f05918da0
shinigamirin <americanxuy@gmail.com>
2024-04-09 17:15:40 +0300

    filled in the data for the SignIn screen

--- app/src/main/java/ua/edu/lntu/cw_1_2/SignInViewModel.kt
+++ app/src/main/java/ua/edu/lntu/cw_1_2/SignInViewModel.kt
@@ -1,2 +1,10 @@
 package ua.edu.lntu.cw_1_2
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    val email = mutableStateOf("")
+    val password = mutableStateOf("")
+    val result = mutableStateOf("")
+}
