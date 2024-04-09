e154eb46eb8b15f607942460571bfae9a735f3c3
shinigamirin <americanxuy@gmail.com>
2024-04-09 17:57:26 +0300

    fill in file for route descriptions

--- app/src/main/java/ua/edu/lntu/cw_1_2/SignInScreenRoute.kt
+++ app/src/main/java/ua/edu/lntu/cw_1_2/SignInScreenRoute.kt
@@ -1,2 +1,14 @@
 package ua.edu.lntu.cw_1_2
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+fun NavGraphBuilder.signInScreen(
+    viewModel: SignInViewModel,
+    onSignInSuccess: () -> Unit
+) {
+    composable("signIn") {
+        SignInScreen(viewModel = viewModel)
+    }
+}
